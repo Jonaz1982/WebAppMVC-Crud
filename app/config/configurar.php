0c5bc169895ba85a409b6ae29e105f33b1d7b100
Johnatan Gonzalez Velez <jgonzalez@sistecredito.com>
2020-05-18 14:31:13 -0500

    Configuracion ala base de datos

--- app/config/configurar.php
+++ app/config/configurar.php
@@ -1,14 +1,14 @@
 <?php
-//Configuracion de acceso ala base de datos
+//Configuracion de acceso a la base de datos
 define('DB_HOST','localhost');
 define('DB_USUARIO','root');
-define('DB_PASSWORD','_SUPASSWORD');
-define('DB_NOMBRE','webappmvc');
+define('DB_PASSWORD','');
+define('DB_NOMBRE','crud_mvc');
 
 
 //Ruta de la aplicacion
-define('RUTA_APP', dirname(dirname(__FILE__)));
+define('RUTA_APP', dirname(__FILE__));
 //Rutade la url : http://localhost:82/WebAppMVC/
-define('RUTA_URL', 'http://localhost:82/WebAppMVC');
+define('RUTA_URL', 'http://localhost:82/WebAppMVC-Crud');
 
-define('NOMBRESITIO', '_NOMBRE_SITIO');
+define('NOMBRESITIO', 'CRUD MVC CON PHP');
