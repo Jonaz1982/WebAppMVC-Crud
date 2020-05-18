0170a05a959f8ee5cc31b8b310e8efc5ac215e3f
Johnatan Gonzalez Velez <jgonzalez@sistecredito.com>
2020-05-18 14:38:36 -0500

    Cargo las vitas que se evidencian en el index o pantalla principal

--- app/librerias/Controlador.php
+++ app/librerias/Controlador.php
@@ -14,7 +14,9 @@
                 //chequear  si el archivo vista existe
                 if (file_exists('../app/vistas/'.$vista.'.php')) {
                     //cargar vista
+                    require_once '../app/vistas/inc/header.php';
                     require_once '../app/vistas/'.$vista.'.php';
+                    require_once '../app/vistas/inc/footer.php';
                 }else{
                     //Si el archivo de la vista no existe
                     die('la vista no existe por favor revisar');
