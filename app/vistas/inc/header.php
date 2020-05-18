786070841bffb4b9a9494d22dd4938bd9aefa05b
Johnatan Gonzalez Velez <jgonzalez@sistecredito.com>
2020-05-18 14:41:30 -0500

    se agrega Boostrap 4 al proyecto para un nav

--- app/vistas/inc/header.php
+++ app/vistas/inc/header.php
@@ -4,8 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="id=edge">
-    <link rel="stylesheet"type="text/css" href="<?php echo RUTA_URL?>/public/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title><?php echo NOMBRESITIO;?></title>
 </head>
 <body>
-    
+   <div class="container">
+   <nav class="navbar navbar-dark bg-dark">
+   <a class="navbar-brand" href="#">MVC PARA UN CRUD EN PHP</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a href=<?php echo RUTA_URL; ?>"" class="navbar-brand">CRUD MVC -CON PHP</a>
+  <div class="collapse navbar-collapse" id= "navbarExampleDefault">
+      <ul class="navbar-nav mr-auto">
+          <li class="nav-item float-right">
+              <a class="nav-link" href="paginas/agregar">
+      </ul>
+  </div>
+   </nav>
+   </div> 
